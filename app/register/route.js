1384e6c56cbf339636370e014968bd5f0311453d
tyronepost <tyrone.post.a@gmail.com>
2016-09-30 19:59:27 -0400

    implemented register.  added ember-cli-mirage, created user model

--- app/register/route.js
+++ app/register/route.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    addNew() {
+      const { controller } = this;
+      const name = controller.get('name');
+      let user = this.store.createRecord('user',{ name: name });
+      user.save().then(() => {
+        console.log('save successful');
+        const message = `A new user with the name ${name} was added`;
+        controller.set('message', message);
+      }, function() {
+        console.log('save failed');
+      });
+   }
+  }
 });
