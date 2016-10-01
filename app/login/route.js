78e82039885d14f48a837c77a4c577a886a2bccc
tyronepost <tyrone.post.a@gmail.com>
2016-10-01 06:59:45 -0400

    refactoring login route to make more readable

--- app/login/route.js
+++ app/login/route.js
@@ -3,17 +3,23 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     login() {
-      const { controller } = this;
+      const { controller, store } = this;
       const name = controller.get('name');
-      this.store.query('user', { name: name }).then((users) => {
-        if (users.get('length') === 1) {
-          let user = users.objectAt(0);
-          this.controllerFor('application').set('user', user);
-          this.transitionTo('notebooks');
-        } else {
-          console.log('unexpected query result');
-        }
+      const user = { name: name };
+      store.query('user', user)
+        .then((users) => {
+          if (users.get('length') === 1) {
+            let user = users.objectAt(0);
+            this._setUserSession.call(this, user);
+          } else {
+            console.log('unexpected query result');
+          }
       });
     }
+  },
+
+  _setUserSession(user) {
+    this.controllerFor('application').set('user', user);
+    this.transitionTo('notebooks');
   }
 });
