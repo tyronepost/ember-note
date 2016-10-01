83f205409840a3ef08de448b049cf0efeb4c9139
tyronepost <tyrone.post.a@gmail.com>
2016-10-01 15:22:12 -0400

    fixed mirage route which broke login acceptance test

--- mirage/config.js
+++ mirage/config.js
@@ -1,34 +1,17 @@
 export default function() {
+
+  // users
   this.get('users', (schema) => {
     return schema.users.all();
   });
+
   this.post('/users');
-  
-  this.get('notebooks?user=:id', (schema, request) => {
-    const id = request.params.id;
-    return schema.notebooks.find(id);
+
+  // notebooks
+  this.get('notebooks', (schema, request) => {
+    const id = request.queryParams.user;
+    const user =  schema.users.find(id);
+    //return user.notebooks.all();
+    return schema.notebooks.where(user);
   });
-  // These comments are here to help you get started. Feel free to delete them.
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
-  */
 }
