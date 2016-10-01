c74695f4d5699fb9c31142deb078f74a4591daed
tyronepost <tyrone.post.a@gmail.com>
2016-09-30 20:15:59 -0400

    adding acceptance test register-test

---
+++ tests/acceptance/register-test.js
@@ -0,0 +1,20 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'ember-note/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | register');
+
+test('visiting /register', function(assert) {
+  visit('/register');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/register');
+  });
+
+  fillIn('.name', 'tyrone');
+  click('.add');
+
+  andThen(function() {
+    const expected = 'A new user with the name tyrone was added';
+    assert.equal(find('.message').text().trim(), expected);
+  });
+});
