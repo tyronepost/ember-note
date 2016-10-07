a0a2e385bbb7603642c969065bc512aa48ae5699
tyronepost <tyrone.post.a@gmail.com>
2016-10-07 12:45:23 -0400

    starting acceptance test for adding a note

---
+++ tests/acceptance/note-test.js
@@ -0,0 +1,21 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'ember-note/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | note');
+
+test('visiting /note', function(assert) {
+  visit('/');
+  click('.register');
+  fillIn('.name', 'tyrone');
+  click('.add');
+  click('.login');
+  fillIn('.name', 'tyrone');
+  click('.login_btn');
+  fillIn('.title', 'notebook 1');
+  click('.add-notebook');
+  click('[data-test-id=1]');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/notebooks/1/notes/1');
+  });
+});
