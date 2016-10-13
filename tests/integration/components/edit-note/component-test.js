8fc9811b8372b0a49952b1a50d3c0e848599daae
tyronepost <tyrone.post.a@gmail.com>
2016-10-12 20:16:47 -0400

    adding component test. changed testem to use firefox in dev env

--- tests/integration/components/edit-note/component-test.js
+++ tests/integration/components/edit-note/component-test.js
@@ -1,12 +1,19 @@
 import { moduleForComponent, test } from 'ember-qunit';
-//import hbs from 'htmlbars-inline-precompile';
+import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('edit-note', 'Integration | Component | edit note', {
   integration: true
 });
 
 test('it renders', function(assert) {
+  this.set('model', { title: 'note 1'});
+  this.render(
+    hbs`{{#edit-note note=model}}
+          Notebook: Notebook 1
+        {{/edit-note}}`
+  );
 
-  assert.ok(true);
-
+  let textareas = this.$('textarea');
+  assert.equal(textareas.length, 1);
+  assert.equal(this.$('input').val(), 'note 1');
 });
